2a863cdf7cff8d0d17a4859c02eb313e48a94d8c
Sebastian009w <ia3266595@gmail.com>
2023-05-27 21:46:30 -0500

    primeros pasos

---
+++ firsttest/suma.go
@@ -0,0 +1,5 @@
+package firsttest
+
+func Sum(a, b int) int {
+	return a + b
+}
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/sebastian009w/go-tests-tutorial/firsttest"
+)
 
 func main() {
-	fmt.Println("Testing")
+	add := firsttest.Sum(2, 3)
+
+	if add != 5 {
+		fmt.Printf("Error: Se esperaba %d, Se obtuvo %d", 5, add)
+		os.Exit(1)
+	}
+	fmt.Println("Succesfuly")
 }
