7717f85e873c00cee49cf1569c15826f8a1f5545
Sebastian009w <ia3266595@gmail.com>
2023-05-27 21:58:38 -0500

    primer test #3

--- firsttest/suma_test.go
+++ firsttest/suma_test.go
@@ -1,1 +1,14 @@
 package firsttest
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	want := 5
+	got := Sum(3, 2)
+
+	if got != want {
+		t.Logf("Error: Se esperaba %d, se obtuvo: %d", want, got)
+		t.Fail()
+	}
+
+}
--- main.go
+++ main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/sebastian009w/go-tests-tutorial/firsttest"
 )
 
 func main() {
-	add := firsttest.Sum(2, 3)
 
-	if add != 5 {
-		fmt.Printf("Error: Se esperaba %d, Se obtuvo %d", 5, add)
-		os.Exit(1)
-	}
-	fmt.Println("Succesfuly")
+	fmt.Println("Todo Salio bien")
 }
