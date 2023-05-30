5e1709146f46a0dd71e4fd9b10e728c6d2a2f36f
Sebastian009w <ia3266595@gmail.com>
2023-05-29 20:40:18 -0500

    second tests

--- fibonacci/fibonacci_test.go
+++ fibonacci/fibonacci_test.go
@@ -4,12 +4,23 @@ import (
 	"testing"
 )
 
-func TestFibonacci(t *testing.T) {
+func TestFibonacciFor(t *testing.T) {
 	want := 55
 	got := FibonacciFor(10)
 
 	if got != want {
 		t.Errorf("FibonacciFor(10) = %d; want %d", got, want)
+	}
+
+}
+
+func TestFibonacciRec(t *testing.T) {
+
+	want := 55
+	got := FinonacciRec(10)
+
+	if got != want {
+		t.Errorf("Se esperaba  %d, se obtuvo %d", want, got)
 	}
 
 }
